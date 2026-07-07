419a583cb505e61f1f884872c2adc1ebbf1cbc5f
CatMoon2626 <r_jiro_2005@i.softbank.jp>
2026-07-07 12:47:42 +0900

    level5の攻略

--- advanced_programming_signal_processing/run.sh
+++ advanced_programming_signal_processing/run.sh
@@ -6,23 +6,45 @@ for image in $1/test/*.ppm; do
     name="imgproc/"$bname
     x=0    	#
     echo $name
-    convert "${image}" "${name}"  # 何もしない画像処理
+#   convert  "${image}" "${name}"  # 何もしない画像処理
 #   convert -blur 2x6 "${image}" "${name}"
-#   convert -median 3 "${image}" "${name}"
+#   convert  -median 1 -equalize  "${image}" "${name}"
 #   convert -auto-level "${image}" "${name}"
-#   convert -equalize "${image}" "${name}"
+    convert -contrast "${image}" "${name}"
+    for size in 50 100 200; 
+    do
+        per="%"
+        size_str="${size}%"
+        echo "${size}"
     rotation=0
     echo $bname:
     for template in $1/*.ppm; do
 	echo `basename ${template}`
-	if [ $x = 0 ]
+    tname=`basename ${template}`
+    resize_template="imgproc/"$tname
+    #limit=$(echo "1.5 * 100 / ${size}" |bc)
+    if [ $size -lt 100 ]
+    then
+    convert -median 2 -scale ${size_str} "${template}" "${resize_template}"
+    if [ $x = 0 ]
 	then
-	    ./matching $name "${template}" $rotation 0.5 cp 
+	    ./matching $name "${resize_template}" $rotation 1.5 cp 
 	    x=1
 	else
-	    ./matching $name "${template}" $rotation 0.5 p 
+	    ./matching $name "${resize_template}" $rotation 1.5 p 
+	fi
+    else
+    convert -scale ${size_str} "${template}" "${resize_template}"
+    fi
+	if [ $x = 0 ]
+	then
+	    ./matching $name "${resize_template}" $rotation 0.5 cp 
+	    x=1
+	else
+	    ./matching $name "${resize_template}" $rotation 0.5 p 
 	fi
     done
     echo ""
+done
 done
 wait
